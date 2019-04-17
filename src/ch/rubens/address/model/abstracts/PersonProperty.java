151d1a6757e2d46e57314fc0d12147c85394b759
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-04-17 08:51:26 -0300

    Adiciona a Interface PersonProperty
    A interface serve para "padronizar" as assinaturas dos métodos que ConcreatePersonProperty usa. Além disso, ela faz com que a classe passe a seguir DIP e OCP.

---
+++ src/ch/rubens/address/model/abstracts/PersonProperty.java
@@ -0,0 +1,27 @@
+package ch.rubens.address.model.abstracts;
+
+import java.time.LocalDate;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * A interface PersonProperty serve para fazer com que a classe 
+ * ConcreatePersonProperty passe a seguir o DIP e tenha um contrato em seus métdos
+ * @author rubens
+ */
+public interface PersonProperty extends Person {
+
+    public StringProperty getFirstNameProperty();
+    
+    public StringProperty getLastNameProperty();
+    
+    public StringProperty getStreetProperty();
+    
+    public StringProperty getCityProperty();
+    
+    public IntegerProperty getPostalCodeProperty();
+    
+    public ObjectProperty<LocalDate> getBirthdayProperty();
+    
+}
