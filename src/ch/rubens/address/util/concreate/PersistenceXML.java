89161cf95fb903991a537ca0e008a374d8051988
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-07-17 17:29:26 -0300

    Implementa o Proxy de persistencia XML

--- src/ch/rubens/address/util/concreate/PersistenceXML.java
+++ src/ch/rubens/address/util/concreate/PersistenceXML.java
@@ -1,43 +1,84 @@
-/*
+
 package ch.rubens.address.util.concreate;
 
 import ch.rubens.address.MainApp;
 import ch.rubens.address.model.abstracts.IListWrapper;
+import ch.rubens.address.model.concreate.PersonListSingleton;
 import ch.rubens.address.util.abstracts.IPersistenceFormat;
 import ch.rubens.address.util.abstracts.PersistenceService;
+import java.io.File;
+import java.util.List;
+import javax.xml.bind.JAXBException;
 
-
-public class PersistenceXML extends PersistenceService {
+/**
+ *
+ * @author rubens
+ */
+public class PersistenceXML extends PersistenceService<File, File> {
     
-    private String context;
+    private Class context;
     private IListWrapper wrapper;
     private MainApp main;
 
-    public PersistenceXML(String context, IListWrapper wrapper, MainApp main) {
+    public PersistenceXML(Class context, IListWrapper wrapper, MainApp main) {
         
         this.context = context;
         this.wrapper = wrapper;
         this.main = main;
         
+        setPersistenceFormat(createPersistenceData());
+        
     }
     
     @Override
-    public void save() {
+    public void save(File data) {
         
-        //getPersistenceFormat().save();
+        getPersistenceFormat().save(data);
         
     }
 
     @Override
-    public void load() {
+    public void load(File data) {
         
-        //getPersistenceFormat().load();
+        List personsSavedList = getPersistenceFormat().load(data);
+        
+        PersonListSingleton.getInstance().clear();
+        
+        if (personsSavedList != null)
+            PersonListSingleton.getInstance().addAll(personsSavedList);
         
     }
 
     @Override
     protected IPersistenceFormat createPersistenceData() {
-        //return new PersistDataXML(context, wrapper, main);
+        
+        try {
+            
+            return new PersistDataXML(context, wrapper, main);
+            
+        } catch (JAXBException ex) {
+            
+            System.out.println("Erro ao definir a forma de salvar os dados do sistema: " + ex);
+            
+        }
+        
+        return null;
+        
+    }
+  
+    public File getFilePath() {
+        
+        PersistDataXML persistFormat = (PersistDataXML) getPersistenceFormat();
+        
+        return persistFormat.getFilePath();
+        
     }
     
-}*/
+    public void resetFilePath() {
+        
+        PersistDataXML persistFormat = (PersistDataXML) getPersistenceFormat();
+        persistFormat.setFilePath(null);
+        
+    }
+    
+}
