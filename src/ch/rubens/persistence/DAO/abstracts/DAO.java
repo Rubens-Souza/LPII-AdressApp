102c6f57b10775bddad156fdc02e126179989c80
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-12 11:05:34 -0300

    Cria interface DAO genérica

---
+++ src/ch/rubens/persistence/DAO/abstracts/DAO.java
@@ -0,0 +1,16 @@
+package ch.rubens.persistence.DAO.abstracts;
+
+import java.util.List;
+
+/**
+ *
+ * @author Aluno
+ */
+public interface DAO<T> {
+    
+    public List<T> listAll();
+    public T add(T data);
+    public T remove(T data);
+    public T update(T oldData, T newData);
+    
+}
