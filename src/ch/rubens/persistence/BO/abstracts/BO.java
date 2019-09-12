49ef449f67ea8c2c5194b08b923a538ae2de4d55
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-12 11:54:58 -0300

    Cria classe BO genérica

---
+++ src/ch/rubens/persistence/BO/abstracts/BO.java
@@ -0,0 +1,17 @@
+package ch.rubens.persistence.BO.abstracts;
+
+import java.util.List;
+
+/**
+ *
+ * @author Aluno
+ */
+public interface BO<T> {
+    
+    public List<T> listAll();
+    public T add(T data);
+    public T remove(T data);
+    public T update(T oldData, T newData);
+    public boolean isRegistered(T data);
+    
+}
