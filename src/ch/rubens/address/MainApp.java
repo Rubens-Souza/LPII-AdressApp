bf06e662d9a563c5e2b65cf587d876beeaf633e9
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-09-19 17:44:44 -0300

    Adapt main para testes

--- src/ch/rubens/address/MainApp.java
+++ src/ch/rubens/address/MainApp.java
@@ -8,9 +8,12 @@ import ch.rubens.address.windows.concreate.PrimaryStageSingleton;
 import ch.rubens.persistence.XML.ContactsFileXMLSingleton;
 import ch.rubens.persistence.XML.DAO.PersonXMLDAO;
 import ch.rubens.persistence.exceptions.NotRegisteredDataException;
+import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javax.xml.parsers.ParserConfigurationException;
@@ -30,10 +33,51 @@ import org.xml.sax.SAXException;
 public class MainApp extends Application {
     
     private IPersonListSingleton personsList;
+    private ContactsFileXMLSingleton contactsFile;
     
-    public MainApp() {
+    public MainApp() throws ParserConfigurationException, SAXException, IOException, TransformerException {
         
         personsList = PersonListSingleton.getInstance();
+        
+        try {
+            
+            contactsFile = ContactsFileXMLSingleton.openXMLFile("Contacts_XML_Saved_Files\\contacts.xml");
+            
+        }
+        catch (ParserConfigurationException | SAXException | IOException ex) {
+            
+            System.out.println("Erro when opening file: " + ex);
+            
+        }
+        
+        Person p1 = new Person(1);
+        Address ad = new Address(31545040);
+        Address ad2 = new Address(31545000);
+        
+        ad.setCity("Belo Horizonte");
+        ad.setStreet("Rua Onofre Camillo Campos");
+        ad2.setCity("Valfeno");
+        ad2.setStreet("Riverdell");
+        
+        p1.setFirstName("Raine");
+        p1.setLastName("Souza");
+        p1.setBirthday(LocalDate.of(2001, Month.MARCH, 2));
+        p1.addAddress(ad);
+        p1.addAddress(ad2);
+        
+        PersonXMLDAO pDAO = new PersonXMLDAO();
+        
+        pDAO.add(p1);
+        
+        contactsFile.saveFileXML();
+        
+        Person p = pDAO.getPerson(p1.getId());
+        System.out.println(p.getFirstName());
+        System.out.println(p.getLastName());
+        System.out.println(p.getAddress(0).getStreet());
+        
+        System.out.println(pDAO.isRegistered(1));
+        System.out.println(pDAO.isRegistered(0));
 
     }
     
@@ -47,74 +91,8 @@ public class MainApp extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, TransformerException, NotRegisteredDataException {
-        
-        Person p1 = new Person(1);
-        Address ad = new Address(31545040);
-        
-        ad.setCity("Belo Horizonte");
-        ad.setStreet("Rua Onofre Camillo Campos");
-        
-        p1.setFirstName("Raine");
-        p1.setLastName("Souza");
-        p1.setBirthday(LocalDate.of(2001, Month.MARCH, 2));
-        p1.addAddress(ad);
-        
-        ContactsFileXMLSingleton contacts = ContactsFileXMLSingleton.openXMLFile("Contacts_XML_Saved_Files\\contacts.xml");
-        
-        Document dc = contacts.getContactsAsDOM();
-
-        /*Element persons = dc.createElement("persons");
-        dc.appendChild(persons);
-        
-        Element el = dc.createElement("person");
-        Attr idAttribute = dc.createAttribute("id");
-        idAttribute.setValue(p1.getId().toString());
-        el.setAttributeNode(idAttribute);
-        persons.appendChild(el);      
-        
-        Element personId = dc.createElement("id");
-        personId.appendChild(dc.createTextNode(p1.getId().toString()));
-        el.appendChild(personId);
-        
-        Element firstName = dc.createElement("firstName");
-        firstName.appendChild(dc.createTextNode(p1.getFirstName()));
-        el.appendChild(firstName);
-        
-        Element lastName = dc.createElement("lastName");
-        lastName.appendChild(dc.createTextNode(p1.getLastName()));
-        el.appendChild(lastName);
-        
-        Element birthday = dc.createElement("birthday");
-        birthday.appendChild(dc.createTextNode(p1.getBirthday().toString()));
-        el.appendChild(birthday);
-        
-        Element address = dc.createElement("address");
-        Attr postalCodeAttribute = dc.createAttribute("postalCode");
-        postalCodeAttribute.setValue(p1.getAddress(0).getPostalCode().toString());
-        address.setAttributeNode(postalCodeAttribute);
-        el.appendChild(address);
-        
-        Element postalCode = dc.createElement("postalCode");
-        postalCode.appendChild(dc.createTextNode(p1.getAddress(0).getPostalCode().toString()));
-        address.appendChild(postalCode);
-        
-        Element city = dc.createElement("city");
-        city.appendChild(dc.createTextNode(p1.getAddress(0).getCity()));
-        address.appendChild(city);
-        
-        Element street = dc.createElement("street");
-        street.appendChild(dc.createTextNode(p1.getAddress(0).getStreet()));
-        address.appendChild(street);*/
-        
-        contacts.saveFileXML();
-        
-        PersonXMLDAO pDAO = new PersonXMLDAO();
-        Person p = pDAO.getPerson(1);
-        
-        System.out.println(pDAO.isRegistered(1));
-        System.out.println(pDAO.isRegistered(0));
-        
+    public static void main(String[] args) {
+           
         launch(args);
         
     }
