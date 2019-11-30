e63710660b92711d9618d6d6f45ed47240918290
Rubens Souza <rubensvictor.m.s@gmail.com>
2019-11-29 22:06:44 -0300

    Cria uma classe para representar o AddressProperty de uma Person

---
+++ src/ch/rubens/address/model/concreate/AddressPropertyAdapter.java
@@ -0,0 +1,78 @@
+package ch.rubens.address.model.concreate;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author rubens
+ */
+public class AddressPropertyAdapter {
+    
+    private Address address;
+    
+    private StringProperty street;
+    private StringProperty city;
+    private IntegerProperty postalCode;
+    
+    public AddressPropertyAdapter(Address address) {
+        
+        setAddress(address);
+        
+    }
+    
+    private void setAddress(Address address) {
+        
+        this.address = address;
+        
+        postalCode = new SimpleIntegerProperty(this.address.getPostalCode());
+        street = new SimpleStringProperty(this.address.getStreet());
+        city = new SimpleStringProperty(this.address.getStreet());
+        
+    }
+    
+    public StringProperty getStreetProperty() {
+        
+        return street;
+        
+    }
+    
+    public StringProperty getCityProperty() {
+        
+        return city;
+        
+    }
+    
+    public IntegerProperty getPostalCodeProperty() {
+        
+        return postalCode;
+        
+    }
+    
+    public String getStreet() {
+        
+        return street.get();
+        
+    }
+    
+    public String getCity() {
+        
+        return city.get();
+        
+    }
+    
+    public Integer getPostalCode() {
+        
+        return postalCode.get();
+        
+    }
+    
+    public Address getAddress() {
+        
+        return address;
+        
+    }
+    
+}
